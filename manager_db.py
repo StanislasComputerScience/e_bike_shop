c47d04fba6cdb52cd6438815107815efbc05fef3
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 09:57:48 +0200

    feat: :sparkles: create function: create_database ()

--- manager_db.py
+++ manager_db.py
@@ -1,7 +1,5 @@
 import sqlite3
 
-ecommerce_db_dict = {}
-
 def create_json_db():
     """Function permit to create json db
     """
@@ -47,8 +45,26 @@ def create_table_db(ecommerce_db_dict: dict):
     ecommerce_db_dict["create_table_connexion"] = """CREATE TABLE Connexion (id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
                                                                              connexion_name TEXT NOT NULL);"""
 
+def create_database(ecommerce_db_dict: dict, database_name: str):
+    """Create your database
+
+    Args:
+        ecommerce_db_dict (dict): contain all queries to create your database
+        database_name (str): database name
+    """
+    try:
+        connexion = sqlite3.connect(f"{database_name}.db")
+        cursor = connexion.cursor()
+        for key, sql_command in ecommerce_db_dict.items():
+            print(key)
+            cursor.execute(sql_command)
+    except:
+         print(f"{database_name} already exist !")
+
 def main():
-    pass
+    ecommerce_db_dict = {}
+    create_table_db(ecommerce_db_dict)
+    create_database(ecommerce_db_dict, "ecommerce_database")
 
 if __name__ == "__main__":
         main()    
