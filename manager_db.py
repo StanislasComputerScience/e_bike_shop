43aaff9b3ad863e7b712db88f0f1295a3750dda6
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 13:08:10 +0200

    feat: :sparkles: add queries: insert lines into tables: role and connection

--- manager_db.py
+++ manager_db.py
@@ -622,11 +622,12 @@ def create_database(ecommerce_db_dict: dict, database_name: str):
         database_name (str): database name
     """
     try:
-        connexion = sqlite3.connect(f"{database_name}.db")
-        cursor = connexion.cursor()
+        Connection = sqlite3.connect(f"{database_name}.db")
+        cursor = Connection.cursor()
         for key, sql_command in ecommerce_db_dict.items():
             print(key)
             cursor.execute(sql_command)
+        Connection.commit()
     except:
         print(f"{database_name} already exist !")
 
