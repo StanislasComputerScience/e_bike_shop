1271f2a61029734c2dfad826b80ed03a2eafc06f
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 13:08:10 +0200

    feat: :sparkles: add queries: insert lines into tables: role and connection

--- manager_db.py
+++ manager_db.py
@@ -9,27 +9,27 @@ def create_table_db(ecommerce_db_dict: dict):
     """
     ecommerce_db_dict["create table Role"] = \
         """CREATE TABLE IF NOT EXISTS Role (
-            id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_role INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             name TEXT NOT NULL);"""
-    ecommerce_db_dict["create table Connexion"] = \
-        """CREATE TABLE IF NOT EXISTS Connexion (
-            id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
+    ecommerce_db_dict["create table Connection"] = \
+        """CREATE TABLE IF NOT EXISTS Connection (
+            id_connection INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             status TEXT NOT NULL);"""
     ecommerce_db_dict["create table User"] = \
         """CREATE TABLE IF NOT EXISTS User (
-            id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
-            id_connexion INTEGER NOT NULL,
+            id_user INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            id_connection INTEGER NOT NULL,
             id_role INTEGER NOT NULL,
             name TEXT NOT NULL,
             firstname TEXT NOT NULL,
             birth_date TEXT,
             email TEXT NOT NULL,
             phone TEXT,
-            FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
+            FOREIGN KEY(id_connection) REFERENCES Connection(id_connection),
             FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
     ecommerce_db_dict["create table Address"] = \
         """CREATE TABLE IF NOT EXISTS Address (
-            id_address INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_address INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             id_user INTEGER NOT NULL,
             number INTEGER NOT NULL,
             street TEXT NOT NULL,
@@ -63,13 +63,13 @@ def create_table_db(ecommerce_db_dict: dict):
         )"""
     ecommerce_db_dict["create table ShoppingCart"] = \
         """CREATE TABLE IF NOT EXISTS ShoppingCart (
-            id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             id_user INTEGER NOT NULL,
             date TEXT NOT NULL,
             FOREIGN KEY(id_user) REFERENCES User(id_user));"""
     ecommerce_db_dict["create table Invoice"] = \
         """CREATE TABLE IF NOT EXISTS Invoice (
-            id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_invoice INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             id_shoppingcart INTEGER NOT NULL,
             date TEXT NOT NULL,
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
@@ -81,9 +81,31 @@ def create_table_db(ecommerce_db_dict: dict):
             quantity INTEGER UNSIGNED NOT NULL,
             rate_vat REAL UNSIGNED NOT NULL,
             PRIMARY KEY(id_prod,id_shoppingcart)
-            FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
+            FOREIGN KEY(id_prod) REFERENCES Product(id_prod)
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
        )"""
+    
+def create_insert_into_tables(ecommerce_db_dict: dict):
+    ecommerce_db_dict["insert into Role: user"] = \
+        """INSERT INTO Role(name)
+            VALUES('user');
+        """
+    ecommerce_db_dict["insert into Role: admin"] = \
+        """INSERT INTO Role(name)
+            VALUES('admin');
+        """
+    ecommerce_db_dict["insert into Connection: timeout"] = \
+        """INSERT INTO Connection(status)
+            VALUES('timeout');
+        """
+    ecommerce_db_dict["insert into Connection: pending"] = \
+        """INSERT INTO Connection(status)
+            VALUES('pending');
+        """
+    ecommerce_db_dict["insert into Connection: connected"] = \
+        """INSERT INTO Connection(status)
+            VALUES('connected');
+        """
 
 def create_database(ecommerce_db_dict: dict, database_name: str):
     """Create your database
@@ -93,17 +115,19 @@ def create_database(ecommerce_db_dict: dict, database_name: str):
         database_name (str): database name
     """
     try:
-        connexion = sqlite3.connect(f"{database_name}.db")
-        cursor = connexion.cursor()
+        Connection = sqlite3.connect(f"{database_name}.db")
+        cursor = Connection.cursor()
         for key, sql_command in ecommerce_db_dict.items():
             print(key)
             cursor.execute(sql_command)
+        Connection.commit()
     except:
          print(f"{database_name} already exist !")
 
 def main():
     ecommerce_db_dict = {}
     create_table_db(ecommerce_db_dict)
+    create_insert_into_tables(ecommerce_db_dict)
     create_database(ecommerce_db_dict, "ecommerce_database")
 
 if __name__ == "__main__":
