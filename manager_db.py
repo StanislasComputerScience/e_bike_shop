40793b04b6dc138e1ed29d6aa2edbeaaef746df2
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 10:09:07 +0200

    feat: :sparkles: add query to create table: User

--- manager_db.py
+++ manager_db.py
@@ -65,6 +65,26 @@ def create_table_db(ecommerce_db_dict: dict):
             FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
        )"""
+    ecommerce_db_dict[
+        "create_table_role"
+    ] = """CREATE TABLE Role (id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                                                   role_name TEXT NOT NULL);"""
+    ecommerce_db_dict[
+        "create_table_connexion"
+    ] = """CREATE TABLE Connexion (id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                                                             connexion_name TEXT NOT NULL);"""
+    ecommerce_db_dict[
+        "create_table_user"
+    ] = """CREATE TABLE User (id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                                                   id_connexion INTEGER NOT NULL,
+                                                                   id_role INTEGER NOT NULL,
+                                                                   user_name TEXT NOT NULL,
+                                                                   user_firstname TEXT NOT NULL,
+                                                                   birth_date TEXT,
+                                                                   user_email TEXT NOT NULL,
+                                                                   user_phone TEXT,
+                                                                   FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
+                                                                   FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
 
 
 def create_database(ecommerce_db_dict: dict, database_name: str):
