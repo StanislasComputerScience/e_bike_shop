25d309e75d684de5f9330478d68f26aa1b744552
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 11:10:20 +0200

    feat: :bug: Fix query to create table CommandLine

--- manager_db.py
+++ manager_db.py
@@ -8,8 +8,8 @@ def create_json_db():
 def create_table_db(ecommerce_db_dict: dict):
     """Function to create table of the db"""
     ecommerce_db_dict[
-        "create_table_role"
-    ] = """CREATE TABLE Role (
+        "create table Role"
+    ] = """CREATE TABLE IF NOT EXISTS Role (
             id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
             name TEXT NOT NULL);"""
     ecommerce_db_dict[
@@ -40,11 +40,38 @@ def create_table_db(ecommerce_db_dict: dict):
             street TEXT NOT NULL,
             postal_code TEXT NOT NULL,
             city TEXT NOT NULL,
-            FOREIGN KEY(id_user) REFERENCES User(id_user)
-        );"""
+            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
     ecommerce_db_dict[
-        "create_table_shopping_cart"
-    ] = """CREATE TABLE ShoppingCart (
+        "create table Category"
+    ] = """CREATE TABLE IF NOT EXISTS Category (
+            id_category INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            name TEXT NOT NULL
+        )"""
+    ecommerce_db_dict[
+        "create table VAT"
+    ] = """CREATE TABLE IF NOT EXISTS VAT (
+            id_vat INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+            rate REAL UNSIGNED NOT NULL,
+            name TEXT NOT NULL
+        )"""
+    ecommerce_db_dict[
+        "create table Product"
+    ] = """CREATE TABLE IF NOT EXISTS Product (
+            id_prod INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            id_category INTEGER NOT NULL,
+            id_vat INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            number_of_units INTEGER UNSIGNED NOT NULL,
+            description TEXT NOT NULL,
+            tech_specification TEXT,
+            image_path TEXT,
+            price_ET REAL UNSIGNED NOT NULL,
+            FOREIGN KEY(id_category) REFERENCES Category(id_category),
+            FOREIGN KEY(id_vat) REFERENCES VAT(id_vat)
+        )"""
+    ecommerce_db_dict[
+        "create table ShoppingCart"
+    ] = """CREATE TABLE IF NOT EXISTS ShoppingCart (
             id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_user INTEGER NOT NULL,
             date TEXT NOT NULL,
@@ -56,8 +83,19 @@ def create_table_db(ecommerce_db_dict: dict):
             id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_shoppingcart INTEGER NOT NULL,
             date TEXT NOT NULL,
+            FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
+    ecommerce_db_dict[
+        "create table CommandLine"
+    ] = """CREATE TABLE IF NOT EXISTS CommandLine (
+            id_prod INTEGER NOT NULL,
+            id_shoppingcart INTEGER NOT NULL,
+            price_ET REAL UNSIGNED NOT NULL,
+            quantity INTEGER UNSIGNED NOT NULL,
+            rate_vat REAL UNSIGNED NOT NULL,
+            PRIMARY KEY(id_prod, id_shoppingcart)
+            FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
-        );"""
+       )"""
 
 
 def create_database(ecommerce_db_dict: dict, database_name: str):
