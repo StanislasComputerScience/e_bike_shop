23fb52a9c843f5fac0561923e7ca5ecac73fd48f
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 10:25:22 +0200

    feat: :sparkles: add queries to create tables: Address, ShoppingCart and Invoice

--- manager_db.py
+++ manager_db.py
@@ -5,53 +5,21 @@ def create_json_db():
     """Function permit to create json db"""
 
 
-def create_json_db():
-    """Function permit to create json db"""
-
-
 def create_table_db(ecommerce_db_dict: dict):
     """Function to create table of the db"""
     ecommerce_db_dict[
-        "create table Category"
-    ] = """CREATE TABLE IF NOT EXISTS Category (
-            id_category INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-            name TEXT NOT NULL
-        )"""
-    ecommerce_db_dict[
-        "create table VAT"
-    ] = """CREATE TABLE IF NOT EXISTS VAT (
-            id_vat INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-            rate REAL UNSIGNED NOT NULL,
-            name TEXT NOT NULL
-        )"""
-    ecommerce_db_dict[
-        "create table Product"
-    ] = """CREATE TABLE IF NOT EXISTS Product (
-            id_prod INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            id_category INTEGER NOT NULL,
-            id_vat INTEGER NOT NULL,
-            name TEXT NOT NULL,
-            number_of_units INTEGER UNSIGNED NOT NULL,
-            description TEXT NOT NULL,
-            tech_specification TEXT,
-            image_path TEXT,
-            price_ET REAL UNSIGNED NOT NULL,
-            FOREIGN KEY(id_category) REFERENCES Category(id_category),
-            FOREIGN KEY(id_vat) REFERENCES VAT(id_vat)
-        )"""
-    ecommerce_db_dict[
         "create_table_role"
-    ] = """CREATE TABLE IF NOT EXISTS Role (
+    ] = """CREATE TABLE Role (
             id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
             name TEXT NOT NULL);"""
     ecommerce_db_dict[
         "create_table_connexion"
-    ] = """CREATE TABLE IF NOT EXISTS Connexion (
+    ] = """CREATE TABLE Connexion (
             id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
             status TEXT NOT NULL);"""
     ecommerce_db_dict[
         "create_table_user"
-    ] = """CREATE TABLE IF NOT EXISTS User (
+    ] = """CREATE TABLE User (
             id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_connexion INTEGER NOT NULL,
             id_role INTEGER NOT NULL,
@@ -61,84 +29,35 @@ def create_table_db(ecommerce_db_dict: dict):
             email TEXT NOT NULL,
             phone TEXT,
             FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
-            FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
+            FOREIGN KEY(id_role) REFERENCES Role(id_role)
+        );"""
     ecommerce_db_dict[
         "create_table_address"
-    ] = """CREATE TABLE IF NOT EXISTS Address (
+    ] = """CREATE TABLE Address (
             id_address INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_user INTEGER NOT NULL,
             number INTEGER NOT NULL,
             street TEXT NOT NULL,
             postal_code TEXT NOT NULL,
             city TEXT NOT NULL,
-            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
+            FOREIGN KEY(id_user) REFERENCES User(id_user)
+        );"""
     ecommerce_db_dict[
         "create_table_shopping_cart"
-    ] = """CREATE TABLE IF NOT EXISTS ShoppingCart (
+    ] = """CREATE TABLE ShoppingCart (
             id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_user INTEGER NOT NULL,
             date TEXT NOT NULL,
-            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
+            FOREIGN KEY(id_user) REFERENCES User(id_user)
+        );"""
     ecommerce_db_dict[
         "create_table_invoice"
-    ] = """CREATE TABLE IF NOT EXISTS Invoice (
+    ] = """CREATE TABLE Invoice (
             id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_shoppingcart INTEGER NOT NULL,
             date TEXT NOT NULL,
-            FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
-    ecommerce_db_dict[
-        "create table CommandeLine"
-    ] = """CREATE TABLE IF NOT EXISTS CommandLine (
-            id_prod INTEGER NOT NULL,
-            id_shoppingcart INTEGER NOT NULL,
-            price_ET REAL UNSIGNED NOT NULL,
-            quantity INTEGER UNSIGNED NOT NULL,
-            rate_vat REAL UNSIGNED NOT NULL,
-            PRIMARY KEY(id_prod,id_shoppingcart)
-            FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
-       )"""
-    ecommerce_db_dict[
-        "create_table_role"
-    ] = """CREATE TABLE Role (id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                   name TEXT NOT NULL);"""
-    ecommerce_db_dict[
-        "create_table_connexion"
-    ] = """CREATE TABLE Connexion (id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                             status TEXT NOT NULL);"""
-    ecommerce_db_dict[
-        "create_table_user"
-    ] = """CREATE TABLE User (id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                   id_connexion INTEGER NOT NULL,
-                                                                   id_role INTEGER NOT NULL,
-                                                                   name TEXT NOT NULL,
-                                                                   firstname TEXT NOT NULL,
-                                                                   birth_date TEXT,
-                                                                   email TEXT NOT NULL,
-                                                                   phone TEXT,
-                                                                   FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
-                                                                   FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
-    ecommerce_db_dict[
-        "create_table_address"
-    ] = """CREATE TABLE Address (id_address INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                         id_user INTEGER NOT NULL,
-                                                                         number INTEGER NOT NULL,
-                                                                         street TEXT NOT NULL,
-                                                                         postal_code TEXT NOT NULL,
-                                                                         city TEXT NOT NULL,
-                                                                         FOREIGN KEY(id_user) REFERENCES User(id_user));"""
-    ecommerce_db_dict[
-        "create_table_shopping_cart"
-    ] = """CREATE TABLE ShoppingCart (id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                                    id_user INTEGER NOT NULL,
-                                                                                    date TEXT NOT NULL,
-                                                                                    FOREIGN KEY(id_user) REFERENCES User(id_user));"""
-    ecommerce_db_dict[
-        "create_table_invoice"
-    ] = """CREATE TABLE Invoice (id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
-                                                                         id_shoppingcart INTEGER NOT NULL,
-                                                                         date TEXT NOT NULL,
-                                                                         FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
+        );"""
 
 
 def create_database(ecommerce_db_dict: dict, database_name: str):
