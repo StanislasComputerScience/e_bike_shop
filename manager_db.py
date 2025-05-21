b46946de2fc4b21ff455b940b011b84024f7efdc
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 11:33:30 +0200

    feat: :art: update the homogeneity of queries

--- manager_db.py
+++ manager_db.py
@@ -85,14 +85,14 @@ def create_table_db(ecommerce_db_dict: dict):
             date TEXT NOT NULL,
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
     ecommerce_db_dict[
-        "create table CommandLine"
+        "create table CommandeLine"
     ] = """CREATE TABLE IF NOT EXISTS CommandLine (
             id_prod INTEGER NOT NULL,
             id_shoppingcart INTEGER NOT NULL,
             price_ET REAL UNSIGNED NOT NULL,
             quantity INTEGER UNSIGNED NOT NULL,
             rate_vat REAL UNSIGNED NOT NULL,
-            PRIMARY KEY(id_prod, id_shoppingcart)
+            PRIMARY KEY(id_prod,id_shoppingcart)
             FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
             FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
        )"""
