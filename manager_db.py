1ae8cfa7fbc3289cc4204f62071e7ae6e5053d02
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 13:02:36 +0200

    * feat: :sparkles: create all tables of the DB via manager_db.py
    * feat: :sparkles: query to create table: Role and Connexion
    
    * feat: :sparkles: create local main in manager_db.py
    
    * feat: :sparkles: create function: create_database ()
    
    * feat: :sparkles: add query to create table: User
    
    * feat: :sparkles: add queries to create tables: Address, ShoppingCart and Invoice
    
    * feat: :sparkles: Create tables Category, VAT, Product (no values yet)
    
    Create function create_table_db with Category, VAT and Product table creations + test
    
    * feat: :sparkles: Create table CommandLine
    
    Add creation of table CommandLine in create_table_db of manager_db.py. Untest because needs a merge.
    
    * fix: :bug: Product -> CommandLine in CommandLine creation
    
    * feat: :sparkles: query to create table: Role and Connexion
    
    * feat: :sparkles: create local main in manager_db.py
    
    * feat: :sparkles: create function: create_database ()
    
    * feat: :sparkles: add query to create table: User
    
    * feat: :sparkles: add queries to create tables: Address, ShoppingCart and Invoice
    
    * feat: :sparkles: verify if tables not exists
    
    * fix: :twisted_rightwards_arrows: conflict solving after rebase
    
    * feat: :bug: Fix query to create table CommandLine
    
    * feat: :art: update the homogeneity of queries
    
    ---------
    
    Co-authored-by: Cesar Gattano <cesar.gattano@gmail.com>

--- manager_db.py
+++ manager_db.py
@@ -1,10 +1,41 @@
 import sqlite3
 
-ecommerce_db_dict = {}
-
+def create_json_db():
+    """Function permit to create json db
+    """
 
 def create_table_db(ecommerce_db_dict: dict):
-
+    """Function to create table of the db
+    """
+    ecommerce_db_dict["create table Role"] = \
+        """CREATE TABLE IF NOT EXISTS Role (
+            id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name TEXT NOT NULL);"""
+    ecommerce_db_dict["create table Connexion"] = \
+        """CREATE TABLE IF NOT EXISTS Connexion (
+            id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
+            status TEXT NOT NULL);"""
+    ecommerce_db_dict["create table User"] = \
+        """CREATE TABLE IF NOT EXISTS User (
+            id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_connexion INTEGER NOT NULL,
+            id_role INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            firstname TEXT NOT NULL,
+            birth_date TEXT,
+            email TEXT NOT NULL,
+            phone TEXT,
+            FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
+            FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
+    ecommerce_db_dict["create table Address"] = \
+        """CREATE TABLE IF NOT EXISTS Address (
+            id_address INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_user INTEGER NOT NULL,
+            number INTEGER NOT NULL,
+            street TEXT NOT NULL,
+            postal_code TEXT NOT NULL,
+            city TEXT NOT NULL,
+            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
     ecommerce_db_dict["create table Category"] = \
         """CREATE TABLE IF NOT EXISTS Category (
             id_category INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
@@ -30,26 +61,50 @@ def create_table_db(ecommerce_db_dict: dict):
             FOREIGN KEY(id_category) REFERENCES Category(id_category),
             FOREIGN KEY(id_vat) REFERENCES VAT(id_vat)
         )"""
-    ecommerce_db_dict["create table CommandLine"] = \
+    ecommerce_db_dict["create table ShoppingCart"] = \
+        """CREATE TABLE IF NOT EXISTS ShoppingCart (
+            id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_user INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
+    ecommerce_db_dict["create table Invoice"] = \
+        """CREATE TABLE IF NOT EXISTS Invoice (
+            id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_shoppingcart INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart));"""
+    ecommerce_db_dict["create table CommandeLine"] = \
         """CREATE TABLE IF NOT EXISTS CommandLine (
-            id_prod INTEGER PRIMARY KEY NOT NULL,
-            id_shoppingcart INTEGER PRIMARY KEY NOT NULL,
+            id_prod INTEGER NOT NULL,
+            id_shoppingcart INTEGER NOT NULL,
             price_ET REAL UNSIGNED NOT NULL,
             quantity INTEGER UNSIGNED NOT NULL,
             rate_vat REAL UNSIGNED NOT NULL,
+            PRIMARY KEY(id_prod,id_shoppingcart)
             FOREIGN KEY(id_prod) REFERENCES Product(id_prod),
-            FOREIGN KEY(id_shoppingcart) REFERENCES VAT(ShoppingCart)
+            FOREIGN KEY(id_shoppingcart) REFERENCES ShoppingCart(id_shoppingcart)
        )"""
 
+def create_database(ecommerce_db_dict: dict, database_name: str):
+    """Create your database
 
-def test():
-    create_table_db(ecommerce_db_dict)
-    with sqlite3.connect("test.db") as connection:
-        print(connection.total_changes)
-        cursor = connection.cursor()
-        for action, sql_command in ecommerce_db_dict.items():
-            print(f"{action}...")
+    Args:
+        ecommerce_db_dict (dict): contain all queries to create your database
+        database_name (str): database name
+    """
+    try:
+        connexion = sqlite3.connect(f"{database_name}.db")
+        cursor = connexion.cursor()
+        for key, sql_command in ecommerce_db_dict.items():
+            print(key)
             cursor.execute(sql_command)
+    except:
+         print(f"{database_name} already exist !")
+
+def main():
+    ecommerce_db_dict = {}
+    create_table_db(ecommerce_db_dict)
+    create_database(ecommerce_db_dict, "ecommerce_database")
 
 if __name__ == "__main__":
-    test()
+        main()
