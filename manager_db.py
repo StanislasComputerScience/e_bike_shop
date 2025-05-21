dfe3b19510faaa592b266b0f3afe56a4d634f454
Aug-Den <augustin.dendievel@gmail.com>
2025-05-21 10:35:30 +0200

    feat: :sparkles: verify if tables not exists

--- manager_db.py
+++ manager_db.py
@@ -10,7 +10,7 @@ def create_json_db():
 
 
 def create_table_db(ecommerce_db_dict: dict):
-
+    """Function to create table of the db"""
     ecommerce_db_dict[
         "create table Category"
     ] = """CREATE TABLE IF NOT EXISTS Category (
@@ -40,14 +40,47 @@ def create_table_db(ecommerce_db_dict: dict):
             FOREIGN KEY(id_vat) REFERENCES VAT(id_vat)
         )"""
     ecommerce_db_dict[
-        "create table ShoppingCart"
+        "create_table_role"
+    ] = """CREATE TABLE IF NOT EXISTS Role (
+            id_role INTEGER PRIMARY KEY AUTOINCREMENT, 
+            name TEXT NOT NULL);"""
+    ecommerce_db_dict[
+        "create_table_connexion"
+    ] = """CREATE TABLE IF NOT EXISTS Connexion (
+            id_connexion INTEGER PRIMARY KEY AUTOINCREMENT, 
+            status TEXT NOT NULL);"""
+    ecommerce_db_dict[
+        "create_table_user"
+    ] = """CREATE TABLE IF NOT EXISTS User (
+            id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_connexion INTEGER NOT NULL,
+            id_role INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            firstname TEXT NOT NULL,
+            birth_date TEXT,
+            email TEXT NOT NULL,
+            phone TEXT,
+            FOREIGN KEY(id_connexion) REFERENCES Connexion(id_connexion),
+            FOREIGN KEY(id_role) REFERENCES Role(id_role));"""
+    ecommerce_db_dict[
+        "create_table_address"
+    ] = """CREATE TABLE IF NOT EXISTS Address (
+            id_address INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_user INTEGER NOT NULL,
+            number INTEGER NOT NULL,
+            street TEXT NOT NULL,
+            postal_code TEXT NOT NULL,
+            city TEXT NOT NULL,
+            FOREIGN KEY(id_user) REFERENCES User(id_user));"""
+    ecommerce_db_dict[
+        "create_table_shopping_cart"
     ] = """CREATE TABLE IF NOT EXISTS ShoppingCart (
             id_shoppingcart INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_user INTEGER NOT NULL,
             date TEXT NOT NULL,
             FOREIGN KEY(id_user) REFERENCES User(id_user));"""
     ecommerce_db_dict[
-        "create table Invoice"
+        "create_table_invoice"
     ] = """CREATE TABLE IF NOT EXISTS Invoice (
             id_invoice INTEGER PRIMARY KEY AUTOINCREMENT, 
             id_shoppingcart INTEGER NOT NULL,
