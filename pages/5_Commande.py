c393c5a0b7edb0f3a38ff1dcee2e5514f4576f7b
Aug-Den <augustin.dendievel@gmail.com>
2025-05-23 16:24:59 +0200

    feat: :art: Improve structure Commande.py

--- pages/5_Commande.py
+++ pages/5_Commande.py
@@ -1,13 +1,40 @@
 import streamlit as st
 import controller.controller as control
 
-ecommerce_db_name = "ecommerce_database"
 
-st.title("Votre commande")
+def display():
+    ecommerce_db_name = "ecommerce_database"
 
-user_info = control.get_all_info_user(ecommerce_db_name, st.session_state["id_user"])
-st.text(f"Utilisateur: {user_info["name"]} {user_info["firstname"]}")
-st.text(f"Mail: {user_info["email"]}")
-st.text(f"Téléphone: {user_info["phone"]}")
+    # Request to the DB
+    if "id_user" in st.session_state:
+        shopping_cart = control.user_shopping_cart(
+            ecommerce_db_name, st.session_state["id_user"]
+        )
+        user_info = control.get_all_info_user(
+            ecommerce_db_name, st.session_state["id_user"]
+        )
+    else:
+        shopping_cart = list()
+        user_info = list()
 
-st.subheader("Panier :")
+    # Display the title
+    st.title("Votre commande")
+
+    # Display User Information
+    display_table_header(user_info)
+
+    # Display the shopping cart as a table
+    column_widths = [1, 2, 2, 1, 1, 1]
+    display_table_header(column_widths)
+
+    st.subheader("Panier :")
+
+
+def display_table_header(user_info: list[dict]) -> None:
+    st.text(f"Utilisateur: {user_info["name"]} {user_info["firstname"]}")
+    st.text(f"Mail: {user_info["email"]}")
+    st.text(f"Téléphone: {user_info["phone"]}")
+
+
+if __name__ == "__main__":
+    display()
