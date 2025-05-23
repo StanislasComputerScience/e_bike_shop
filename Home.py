8e14eb2bddb6258245383eb80a8b117fb5a573da
StanislasComputerScience <remilabonne@yahoo.fr>
2025-05-23 14:30:52 +0200

    fix: :bug: using le variable : ecommerce_db_name

--- Home.py
+++ Home.py
@@ -32,8 +32,8 @@ with colonne_titre:
         unsafe_allow_html=True,
     )
 
-most_popular_products = control.most_popular_products("ecommerce_database")
-most_products_buy = control.most_products_buy("ecommerce_database")
+most_popular_products = control.most_popular_products(ecommerce_db_name)
+most_products_buy = control.most_products_buy(ecommerce_db_name)
 
 st.subheader("Produits les plus populaires :")
 # Créer 2 colonnes
