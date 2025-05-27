3a0a40d1d49e7e827f0d3a0eac1afb95c76f776e
Aug-Den <augustin.dendievel@gmail.com>
2025-05-27 08:57:52 +0200

    fix: :art: Add comment

--- controller/controller.py
+++ controller/controller.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-# Add parent folder au sys.path
+# Add parent folder at the sys.path
 import const_values as cv
 
 
