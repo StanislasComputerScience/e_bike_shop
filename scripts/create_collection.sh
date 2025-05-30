55821116a5d301e7f04447b33de09bdd6b494e59
Aug-Den <augustin.dendievel@gmail.com>
2025-05-30 13:59:30 +0200

    fix: :bug: Fix script/create_collection.sh

--- scripts/create_collection.sh
+++ scripts/create_collection.sh
@@ -1,5 +1,3 @@
 python3 ./bdd/manager_mongodb_product.py
 python3 ./bdd/manager_mongodb_user.py
 python3 ./bdd/manager_mongodb_invoice.py
-python3 ./bdd/manager_mongodb_category.py
-python3 ./bdd/manager_mongodb_vat.py
